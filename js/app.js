0033001b3601c378c2c4b7fbdbffa7a4729d1041
Michael Peters <michaelrespond99@gmail.com>
2022-03-14 08:55:28 -0400

    Proyecto Finalizado

--- js/app.js
+++ js/app.js
@@ -54,12 +54,77 @@ function consultarAPI(ciudad, pais) {
 
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${ciudad},${pais}&appid=${appId}`
 
+    Spinner();
+
     fetch(url)
         .then(respuesta => respuesta.json())
         .then(datos => {
-            console.log(datos);
+           limpiarHTML(); // se limpia el resultado anterior
             if (datos.cod === "404") {
                 mostrarError('Ciudad no encontrada')
             }
+
+            mostrarClima(datos);
         })
+}
+
+function mostrarClima(datos) {
+
+    const {name, main: { temp, temp_max, temp_min } } = datos;
+
+    const centigrados = kelvinACentigrados(temp);
+    const max = kelvinACentigrados(temp_max);
+    const min = kelvinACentigrados(temp_min);
+    const nombreCiudad = document.createElement('p');
+
+    nombreCiudad.textContent = `Clima en ${name}`;
+    nombreCiudad.classList.add('font-bold', 'text-2xl');
+
+    const actual = document.createElement('p');
+    actual.innerHTML = `${centigrados} &#8451`;
+    actual.classList.add ('font-bold','text-6xl')
+
+    const tempMaxima = document.createElement('p');
+    tempMaxima.innerHTML = `Temperatura Máxima: ${max} &#8451`;
+    tempMaxima.classList.add('text-xl');
+
+    const tempMinima = document.createElement('p');
+    tempMinima.innerHTML = `Temperatura Minima: ${min} &#8451`;
+    tempMinima.classList.add('text-xl');
+
+
+    const resultadoDiv = document.createElement('div');
+    resultadoDiv.classList.add('text-center' , 'text-white')
+    resultadoDiv.appendChild(nombreCiudad);
+    resultadoDiv.appendChild(actual);
+    resultadoDiv.appendChild(tempMaxima);
+    resultadoDiv.appendChild(tempMinima);
+
+    resultado.appendChild(resultadoDiv);
+}
+
+
+const kelvinACentigrados = grados => parseInt(grados - 273.15)
+
+
+
+
+function limpiarHTML() {
+    while (resultado.firstChild) {
+        resultado.removeChild(resultado.firstChild);
+    }
+}
+
+function Spinner() {
+    
+    limpiarHTML();
+  
+    const divSpinner = document.createElement('div')
+    divSpinner.classList.add('spinner');
+
+    divSpinner.innerHTML = `
+     <div class="double-bounce1"></div>
+     <div class="double-bounce2"></div>
+    `
+    resultado.appendChild(divSpinner);
 }
